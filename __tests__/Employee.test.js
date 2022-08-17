e55221611f128ca83e80ee5755e09e03a2016c23
evanbruce38 <106777653+evanbruce38@users.noreply.github.com>
2022-08-17 15:08:27 -0400

    first test passed

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,6 +3,7 @@ const Employee = require('../lib/Employee');
 test('set a name', () => {
     const newEmployee = new Employee('Dave', 1, "dave@mail.com");
 
-    expect(newEmployee.name)toBe("Dave");
+    expect(newEmployee.name).toBe("Dave");
 });
+
 
