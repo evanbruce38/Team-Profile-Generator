df4e59016c23bac3081ca40b35bf5f053fa93ce1
evanbruce38 <106777653+evanbruce38@users.noreply.github.com>
2022-08-17 15:19:24 -0400

    finished employee tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -17,3 +17,27 @@ test('set an email', () => {
 
     expect(newEmployee.email).toBe("dave@mail.com");
 });
+
+test('test for the getName method', () => {
+    const newEmployee = new Employee('Dave', 1, "dave@mail.com");
+
+    expect(newEmployee.getName()).toBe("Dave");
+});
+
+test('test for the getId method', () => {
+    const newEmployee = new Employee('Dave', 1, "dave@mail.com");
+
+    expect(newEmployee.getId()).toBe(1);
+});
+
+test('test for the getEmail method', () => {
+    const newEmployee = new Employee('Dave', 1, "dave@mail.com");
+
+    expect(newEmployee.getEmail()).toBe("dave@mail.com");
+});
+
+test('test for the getRole method', () => {
+    const newEmployee = new Employee('Dave', 1, "dave@mail.com");
+
+    expect(newEmployee.getRole()).toBe("Employee");
+});
