97bb0bae2b23d826969fc5e6e11a23035cbf4a03
evanbruce38 <106777653+evanbruce38@users.noreply.github.com>
2022-08-17 16:46:23 -0400

    finished inquirer prompts

--- index.js
+++ index.js
@@ -40,6 +40,87 @@ function addManager() {
 function addTeamMember() {
     inquirer
     .prompt([
-        
+        {
+            type: 'list',
+            name: 'addTeamMember',
+            message: 'What would you like to do next?',
+            choices: ['Add an Engineer', 'Add an Intern', 'Done building my team!']
+        };
     ])
+    .then(answers => {
+        if (answers.addTeamMember == 'Add an Engineer') {
+            addEngineer();
+        } else if (answers.addTeamMember == 'Add an Intern') {
+            addIntern();
+        } else {
+            const generatedHTML = generateHTML(employeeArray);
+            fs.writeFile('./dist/index/html', generatedHTML, () => {
+            console.log('HTML has been succesfully generated!');
+            });
+        };
+    });
+};
+
+function addEngineer() {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'engineerName',
+            message: 'What is the name of this Engineer?'
+        },
+        {
+            type: 'input',
+            name: 'engineerId',
+            message: "What is this Engineer's ID number?"
+        },
+        {
+            type: 'input',
+            name: 'engineerEmail',
+            message: "What is this Engineer's email adress?"
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: "What is this Engineer's github?"
+        }
+    ]);
+    .then(answers => {
+        const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.github);
+        employeeArray.push(engineer);
+        addTeamMember();
+    });
 }
+
+function addIntern() {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'internName',
+            message: 'What is the name of this Intern?'
+        },
+        {
+            type: 'input',
+            name: 'internId',
+            message: "What is this Intern's ID number?"
+        },
+        {
+            type: 'input',
+            name: 'internEmail',
+            message: "What is this Intern's email adress?"
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: "What school does this Intern attend?"
+        }
+    ]);
+    .then(answers => {
+        const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.school);
+        employeeArray.push(intern);
+        addTeamMember();
+    });
+};
+
+addManager();
